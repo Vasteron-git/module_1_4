53751f526999d8e246d83d8477c7cc9b5dc2e7a8
Vasteron-git <Mp.selvester@gmail.com>
2024-08-31 19:38:44 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+
+# 2nd task
+my_string = input("Введите произвольный текст: ")
+print("Количество символов введённого текста:", len(my_string))
+# 3rd task
+print("my_string в верхнем регистре:", my_string.upper())
+print("my_string в нижнем регистре:", my_string.lower())
+print("my_string без пробелов:", my_string.replace(" ",""))
+print("Первый символ строки my_string:", my_string[0])
+print("Последний символ строки my_string:", my_string[len(my_string)-1])
